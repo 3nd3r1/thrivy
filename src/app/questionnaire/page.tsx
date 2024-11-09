8cac0da962a0791b2200d675d4ecbf9adcc25431
3nd3r1 <ender.spam.guru@gmail.com>
2024-11-09 19:10:48 +0200

    fix(questionnaire): Add margin top

--- src/app/questionnaire/page.tsx
+++ src/app/questionnaire/page.tsx
@@ -2,12 +2,12 @@ import QuestionnaireForm from "@/components/questionnaire/questionnaire-form";
 
 const Questionnaire = () => {
     return (
-        <>
+        <div className="mt-8">
             <h1 className="text-2xl font-bold text-center mb-4">
                 Job Satisfaction Questionnaire
             </h1>
             <QuestionnaireForm />
-        </>
+        </div>
     );
 };
 
